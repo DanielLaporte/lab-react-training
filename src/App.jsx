91cdafc5491f26dbd0f7b078b9390cfbc8cb8910
DanielLaporte <daniel.laporte2008@gmail.com>
2023-09-02 12:59:00 +0200

    iniciado

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import IdCard from './components/IdCard'; // Asegúrate de proporcionar la ruta correcta al archivo IdCard.js
+
+const App = () => {
+  return (
+    <div>
+      <IdCard
+        lastName='Doe'
+        firstName='John'
+        gender='male'
+        height={178}
+        birth={new Date("1992-07-14")}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+
+      <IdCard
+        lastName='Delores'
+        firstName='Obrien'
+        gender='female'
+        height={172}
+        birth={new Date("1988-05-11")}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
+    </div>
+  );
+}
+
+export default App;
